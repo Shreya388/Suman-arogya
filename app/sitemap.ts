83702d33d6f0146ff6db478e30481c5b2fffe97d
Shreya388 <meenaxjha@gmail.com>
2026-08-07 16:53:50 +0530

    added services, doctors, blog in sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -13,8 +13,29 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
     {
       url: `${baseUrl}/about`,
       lastModified: new Date(),
-      changeFrequency: 'monthly',
+      changeFrequency: 'weekly',
       priority: 0.8,
     },
+    // your doctors pages here:
+    {
+      url: `${baseUrl}/doctors`, // Or whatever your main doctors page URL is
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 0.9,
+    },
+    // your service pages here:
+    {
+      url: `${baseUrl}/services`, // Or whatever your main services page URL is
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 0.9,
+    },
+    {
+        url: `${baseUrl}/blog`,
+        lastModified: new Date(),
+        changeFrequency: 'weekly',
+        priority: 0.9,
+
+    }
   ];
 }
